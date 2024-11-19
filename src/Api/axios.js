5929685ab2eb69c2cdb96ee67887a79968889209
sena <mymachines0@gmail.com>
2024-11-19 07:50:50 -0800

    updated axios for render.com

--- src/Api/axios.js
+++ src/Api/axios.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL:"http://127.0.0.1:5001/ae-clone-6be8c/us-central1/api"
+    // local instance of firebase functions
+    // baseURL:"http://127.0.0.1:5001/ae-clone-6be8c/us-central1/api"
+    
+    // deployed version of amazon-api-deploy on WebGL2RenderingContext.com
+    baseURL:"https://amazon-api-deploy-naseff.onrender.com/"
+
 });
 
 export {axiosInstance};
